286fa60eaa34619f25bc8fd223063e2cd8cb2f8b
Kaartik <kaartik.1rn17cs041@gmail.com>
2020-05-24 16:36:46 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
+  import React from "react";
+  // import logo from "./Costues-Logo.png"
+  import "./App.css";
 
-import "./App.css";
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello WORLD!</h1>
-    </div>
-  );
-}
-
-export default App;
+  class signin extends React.Component{
+    render()
+    {
+      return(
+        <div className="box">
+          <div className="leftbox">
+            <div className="black" />
+            <div className="image" />
+            <div className="innerbox">
+              <div className="text"><h4><span>LOGIN</span></h4>
+                <div className="inputbox">
+                  <input className="inputs" type="text" placeholder="Username" />
+                </div>
+                <div className="inputbox">
+                  <input className="inputs" type="password" placeholder="Password" />
+                </div>
+                <div className="btn">Login</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )
+    }
+  }
+  export default signin;
