2f358fbc53953b6dac4acf005d544571b157c50d
Kaartik <kaartik.1rn17cs041@gmail.com>
2020-06-01 22:48:30 +0530

    update 4

--- src/signinform.js
+++ src/signinform.js
@@ -1,63 +1,83 @@
 import React from "react";
+import Popup from "reactjs-popup";
 import "./App.css";
-class signinform extends React.Component{
-    constructor(props){
-        super(props)
-
-        this.state={
-            username:"",
-            password:"",
-        }
-        this.handleSubmit=this.handleSubmit.bind(this)
-    }
-    namehandler=(event)=>{
-        this.setState({
-            username: event.target.value
-        })
-    }
-    passwordhandler=(event)=>{
-        this.setState({
-            password: event.target.value
-        })
-    }
-    handleSubmit=(event)=>{
-        // alert("${this state.username} Login Successfully")
-        console.log(this.state);
-        this.setState({
-            username:"",
-            password:"",
-        })
-    event.preventDefault()
-    }
-
-
-    render()
-    {
-        return(
-            <div className="box">
-                <div className="leftbox">
-                <div className="black" />
-                <div className="image" />
-                <div className="innerbox">
-                    <form onSubmit={this.handleSubmit}>
-                        <div className="text"><h4><span>LOGIN</span></h4>
-                            <div className="inputbox">
-                                <input className="inputs" type="text" value={this.state.username} onChange={this.namehandler}placeholder="Username" />
-                            </div>
-                            <div className="inputbox">
-                                <input className="inputs" type="password" value={this.state.password} onChange={this.passwordhandler} placeholder="Password" />
-                            </div>
-                            <input className="btn" type="submit" value="Submit" />
-                            <div className="othertext">
-                                Haven't Registered Yet? Register Now
-                            </div>
+class ControlledPopup extends React.Component {
+            constructor(props) {
+                super(props);
+                this.state = { open: false };
+                this.openModal = this.openModal.bind(this);
+                this.closeModal = this.closeModal.bind(this);
+                this.state={
+                    username:"",
+                    password:"",
+                }
+                this.handleSubmit=this.handleSubmit.bind(this)
+            }
+            openModal() {
+            this.setState({ open: true });
+            }
+            closeModal() {
+            this.setState({ open: false });
+            }
+    
+            namehandler=(event)=>{
+                this.setState({
+                    username: event.target.value
+                })
+            }
+            passwordhandler=(event)=>{
+                this.setState({
+                    password: event.target.value
+                })
+            }
+            handleSubmit=(event)=>{
+                
+                console.log(this.state);
+                this.setState({
+                    username:"",
+                    password:"",
+                })
+            event.preventDefault()
+            }
+        
+        
+            render() {
+            return (
+                <div>
+                <button className="button" onClick={this.openModal}>
+                    Login
+                </button>
+                <Popup
+                    open={this.state.open}
+                    closeOnDocumentClick
+                    onClose={this.closeModal}
+                >
+                    <div className="box">
+                        <div className="leftbox">
+                        <div className="black" />
+                        <div className="image" />
+                        <div className="innerbox">
+                            <form onSubmit={this.handleSubmit}>
+                                <div className="text"><h4><span>LOGIN</span></h4>
+                                    <div className="inputbox">
+                                        <input className="inputs" id="username" type="text" value={this.state.username} onChange={this.namehandler}placeholder="Username" />
+                                    </div>
+                                    <div className="inputbox">
+                                        <input className="inputs" id="password" type="password" value={this.state.password} onChange={this.passwordhandler} placeholder="Password" />
+                                    </div>
+                                    <input className="btn" type="submit" value="Submit" />
+                                    <div className="othertext">
+                                        Haven't Registered Yet? Register
+                                    </div>
+                                </div>
+                            </form>
                         </div>
-                    </form>
+                        </div>
+                    </div>
+                    
+                </Popup>
                 </div>
-                </div>
-            </div>
-        )
+            );
     }
-}
-export default signinform;
-
+  }
+  export default ControlledPopup;
--- src/signupform.js
+++ src/signupform.js
@@ -1,54 +1,73 @@
 import React from "react";
+import Popup from "reactjs-popup";
 import "./App.css";
-class signupform extends React.Component{
-    constructor(props){
-        super(props)
-
-        this.state={
-            firstname:"",
-            lastname:"",
-            email:"",
-            password:"",
-        }
-        this.handleSubmit=this.handleSubmit.bind(this)
-    }
-    firsthandler=(event)=>{
-        this.setState({
-            firstname: event.target.value
-        })
-    }
-    lasthandler=(event)=>{
-        this.setState({
-            lastname: event.target.value
-        })
-    }
-    email=(event)=>{
-        this.setState({
-            email: event.target.value
-        })
-    }
-    passwordhandler=(event)=>{
-        this.setState({
-            password: event.target.value
-        })
-    }
-    handleSubmit=(event)=>{
-        // alert("${this state.username} Login Successfully")
-        console.log(this.state);
-        this.setState({
-            firstname:"",
-            lastname:"",
-            email:"",
-            password:"",
-        })
-    event.preventDefault()
-    }
-
-
-    render()
-    {
-        return(
-            <div className="box">
+class ControlledPopup extends React.Component {
+            constructor(props) {
+                super(props);
+                this.state = { open: false };
+                this.openModal = this.openModal.bind(this);
+                this.closeModal = this.closeModal.bind(this);
+                this.state={
+                    firstname:"",
+                    lastname:"",
+                    email:"",
+                    password:"",
+                }
+                this.handleSubmit=this.handleSubmit.bind(this)
+            }
+            openModal() {
+            this.setState({ open: true });
+            }
+            closeModal() {
+            this.setState({ open: false });
+            }
+    
+            firsthandler=(event)=>{
+                        this.setState({
+                             firstname: event.target.value
+                        })
+                    }
+                    lasthandler=(event)=>{
+                     this.setState({
+                             lastname: event.target.value
+                         })
+                     }
+                     email=(event)=>{
+                         this.setState({
+                             email: event.target.value
+                         })
+                     }
+                     passwordhandler=(event)=>{
+                         this.setState({
+                             password: event.target.value
+                     })
+                     }
+                     handleSubmit=(event)=>{
+                         // alert("${this state.username} Login Successfully")
+                         console.log(this.state);
+                         this.setState({
+                             firstname:"",
+                          lastname:"",
+                            email:"",
+                             password:"",
+                        })
+                     event.preventDefault()
+                     }
+                
+        
+        
+            render() {
+            return (
+                <div>
+                <button className="button" onClick={this.openModal}>
+                    Signup
+                </button>
+                <Popup
+                    open={this.state.open}
+                    closeOnDocumentClick
+                    onClose={this.closeModal} position="right center"
+                >
+                    <div className="box">
                 <div className="leftbox">
                 <div className="black" />
                 <div className="image" />
@@ -56,16 +75,16 @@ class signupform extends React.Component{
                     <form onSubmit={this.handleSubmit}>
                         <div className="textt"><h4><span>SIGNUP</span></h4>
                             <div className="inputbox">
-                                <input className="inputs" type="text" value={this.state.firstname} onChange={this.firsthandler} placeholder="Firstname" />
+                                <input className="inputs" id="firstname" type="text" value={this.state.firstname} onChange={this.firsthandler} placeholder="Firstname" />
                             </div>
                             <div className="inputbox">
-                                <input className="inputs" type="text" value={this.state.lastname} onChange={this.lasthandler} placeholder="Lastname" />
+                                <input className="inputs" id="lastname" type="text" value={this.state.lastname} onChange={this.lasthandler} placeholder="Lastname" />
                             </div>
                             <div className="inputbox">
-                                <input className="inputs" type="text" value={this.state.email} onChange={this.email} placeholder="Email" />
+                                <input className="inputs" id="email" type="text" value={this.state.email} onChange={this.email} placeholder="Email" />
                             </div>
                             <div className="inputbox">
-                                <input className="inputs" type="password" value={this.state.password} onChange={this.passwordhandler} placeholder="Password" />
+                                <input className="inputs" id="password" type="password" value={this.state.password} onChange={this.passwordhandler} placeholder="Password" />
                             </div>
                             <input className="btn" type="submit" value="Submit" />
                             <div className="othertext">
@@ -76,8 +95,11 @@ class signupform extends React.Component{
                 </div>
                 </div>
             </div>
-        )
+                    
+                </Popup>
+                </div>
+            );
     }
-}
-export default signupform;
+  }
+  export default ControlledPopup;
 
